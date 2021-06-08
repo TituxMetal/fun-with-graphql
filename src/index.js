63c8b16e2101b9aeca6061dc95c4a98659021a2b
Titux Metal <github@lgdweb.fr>
2021-06-08 17:30:11 +0200

    Updating index.js file; passes type definition and resolver in createApp function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+import { typeDefs, resolvers } from '#root/graphql'
 import { shutdownServer } from '#root/helpers'
 
 import createApp from './app'
 
-const server = createApp()
+const server = createApp(typeDefs, resolvers)
 
 shutdownServer(server)
