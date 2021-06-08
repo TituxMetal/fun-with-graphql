ccb5f2b38680ae91d4a505c4388674c7be48cfa1
Titux Metal <github@lgdweb.fr>
2021-06-08 17:28:54 +0200

    Adding index.js file in graphql; adds the basic type definition and resolver

---
+++ src/graphql/index.js
@@ -0,0 +1,11 @@
+export const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`
+
+export const resolvers = {
+  Query: {
+    hello: (_, { name }) => `Hello ${name || 'World'} and welcome!`
+  }
+}
