3ca7ee8cf7c3ee4bea775e60009ed3b36a6b1a76
Titux Metal <github@lgdweb.fr>
2021-06-12 09:01:59 +0200

    Updating index.js file in collections; improves getByKey function; adds getAllAndPopulate function that returns an array of documents and linked document by reference to another collection, in other words this mimics the behavior of the populate function from mongoose

--- src/collections/index.js
+++ src/collections/index.js
@@ -1,11 +1,28 @@
+import { ObjectID } from 'mongodb'
+
 import { getCollection } from '#root/database'
 
 export const getByKey = async (collectionName, key = null) => {
   const collection = await getCollection(collectionName)
-  const allData = await collection.find({ key }).toArray()
+  const allData = await collection.find(key).toArray()
   const data = allData.map(({ _id, ...rest }) => ({ id: _id, ...rest }))
 
-  return data
+  return key === null ? data : data[0]
+}
+
+export const getAllAndPopulate = async (collectionName, ref, collection) => {
+  const allData = await getByKey(collectionName)
+
+  const promise = allData.map(async document => {
+    const refObjectId = ObjectID.createFromHexString(document[ref])
+    const dataToPopulateWith = await getByKey(collection, { _id: refObjectId })
+
+    return { ...document, [ref]: dataToPopulateWith }
+  })
+
+  const allPopulatedData = await Promise.all(promise)
+
+  return allPopulatedData
 }
 
 export const createDocument = async (collectionName, data = {}) => {
